f27121e37b2c0132c5305fb3c15f05778aa27188
yosuahohoho <yosuahohoho@gmail.com>
2017-10-11 09:59:19 +0700

    add docstring to functions

--- bin_message.py
+++ bin_message.py
@@ -4,14 +4,14 @@
 text = raw_input("Enter your text message:\n")
 
 def to_binary(msg):
-	
+	""" Convert ASCII characters to binary representation. """
 	output = [format(ord(char), 'b') for char in msg]
 	
 	return ' '.join(output)
 	
 	
 def to_string(msg):
-	
+	""" Convert binary representation in string to ASCII. """
 	binaries = msg.split(' ')
 	output = [chr(int(byte, 2)) for byte in binaries]
 	
